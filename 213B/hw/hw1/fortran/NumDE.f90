75700fd0f11ba7ce8c96b04ababec890e3c1d3ac
Dante Buhl <dbuhl@ucsc.edu>
2024-04-15 18:33:10 -0700

    Added new work for part b

--- 213B/hw/hw1/fortran/NumDE.f90
+++ 213B/hw/hw1/fortran/NumDE.f90
@@ -3,9 +3,10 @@
 !Purpose: To store numerical methods for Differential Equations
 
 module NumDE
+    
+  use LinAl
 
   implicit none
-  
 
   contains 
 
@@ -64,8 +65,43 @@ module NumDE
 
   end subroutine aeRK3
 
-  subroutine AM3(F, num_points, dx)
+  subroutine AM3(F, Y, ma, num_points, dt, num_iter)
+    ! this subroutine can only handle linear ODE's as of right now
+    ! That ism the jacobian matrix, A, is constant
 
+    implicit none
+
+    real :: Y(:, :), A(:, :), dt
+    integer, intent(in) :: ma, num_points, num_iter
+    real, dimension(ma, ma) :: Jac, InvJac
+    real, dimension(ma, 1) :: B, X
+    integer, dimension(ma) :: P
+    integer :: i, j
+    logical :: bool
+
+
+    ! A = I - J_G(u_k+1)
+    call eye(Jac, ma)
+    Jac = Jac - F
+    InvJac = Jac
+    call LU(InvJac, ma, bool, P)
+
+    do j = 1, num_points
+
+        ! implicit step, Fixed Point Iteration for u_k+1
+        B = Y(:, i:i) + dt*matmul(
+        X = 0.0
+        do i = 1, num_iter
+            ! find u_k+1^j+1 = u_k+1^j + (eye - dt*A)^-1(u_k + dt A(u_k+1^j) - u_k+1^j)
+            call LUsolve(InvJac, ma, B, X, 1, P)
+            B = Y(:, i:i) + dt*matmul(F, ) - Y(:, i+1:i+1) + matmul(Jac, Y(:, i:i))
+            Y(:, i+1) = X
+        end do
+    `        
+        ! updating y vectors
+        Y(:, i) = !AM3 Method
+
+    end do
 
   end subroutine AM3
 
