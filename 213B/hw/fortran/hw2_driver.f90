05f2410472722b7f6c04bbbc83db21795b157de4
Dante Buhl <dbuhl@ucsc.edu>
2024-04-25 12:41:31 -0700

    Added stuff

--- 213B/hw/fortran/hw2_driver.f90
+++ 213B/hw/fortran/hw2_driver.f90
@@ -11,20 +11,26 @@ implicit none
 
   integer, parameter :: ma=3, na=3
   real, dimension(ma, na) :: A
-  real :: tol=10.d-14
-  logical :: shift=.false.
+  real :: tol=10.d-8
+  logical :: shift=.true.
 
   A(1, :) = (/0.0, 10.0, -10.0/)
   A(2, :) = (/-100.0, -1.0, 0.0/)
   A(3, :) = (/0.0, 10.0, -100.0/)
-  
-  call eigQR(A, ma, shift, tol) 
+ 
+  !Note: This fails becaause the eigenvalues of this matrix are complex 
+  ! need to upgrade my linal module to handle complex matrices/eigenvectors.
+  !call eigQR(A, ma, shift, tol) 
+  ! Note that the eigenvalues of A are:
+  ! -0.9666 + i 30.1255
+  ! -0.9666 - i 30.1255
+  ! -99.0667 
 
-  print *, " "
-  print *, " Eigenvalues of A: "
-  print *, " ---------------- "
-  print *, " "
-  call printmat(A, ma, na)
-  print *, " "
+  !print *, " "
+  !print *, " Eigenvalues of A: "
+  !print *, " ---------------- "
+  !print *, " "
+  !call printmat(A, ma, na)
+  !print *, " "
 
 end program hw2
