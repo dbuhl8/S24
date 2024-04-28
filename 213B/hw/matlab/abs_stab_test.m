048b03285a3b16c6cd964afb58e4100e2af5a6ed
Dante Buhl <dbuhl@ucsc.edu>
2024-04-28 12:42:43 -0700

    plotted all regions of abs stab

--- 213B/hw/matlab/abs_stab_test.m
+++ 213B/hw/matlab/abs_stab_test.m
@@ -1,5 +1,4 @@
 clc; close; clear;
-
 A = [0.0, 10.0, -10.0; -100.0, -1.0, 0.0; 0.0, 10.0, -100.0];
 
 [V, D] = eig(A);
@@ -45,3 +44,86 @@ for i = 1:3
     plot(xl, yl, 'xr');
     line([xl, 0], [yl, 0]);
 end
+
+figure(2); 
+hold on;
+
+c1 = 23./12.;
+c2 = -16./12.;
+c3 = 5./12.;
+th = 0:0.0001:2*pi;
+r = (exp(complex(0, 2*th)) - 4.*exp(complex(0, th)) + 3.)/2.;
+%(exp(complex(0, 3*th)) - exp(complex(0, 2*th)))./(c1*exp(complex(0,2*th)) + c2*exp(complex(0, th)) + c3);
+
+x = real(r);
+y = imag(r);
+
+plot(x, y);
+
+figure(3); 
+hold on;
+
+th = 0:0.0001:2*pi;
+r = (exp(complex(0, 3*th)) - 18.*exp(complex(0, 2*th)) ...
+    +9.*exp(complex(0, th)) - 2)./(6.*exp(complex(0,3*th)));
+%(exp(complex(0, 2*th)) - 4.*exp(complex(0, th)) + 3.)/2.;
+%(exp(complex(0, 3*th)) - exp(complex(0, 2*th)))./(c1*exp(complex(0,2*th)) + c2*exp(complex(0, th)) + c3);
+
+x = real(r);
+y = imag(r);
+
+plot(x, y);
+
+figure(4); 
+hold on;
+
+%th = 0:0.0001:2*pi;
+b = [2./9, 1./3, 4./9];
+x = -3:0.1:1;
+y = -3:0.1:3;
+[xx, yy] = meshgrid(x, y);
+z = complex(xx, yy); %exp(complex(0., th));
+%z2 = z1.^2; %exp(complex(0., 2.*th));
+%z3 = z1.^3; %exp(complex(0., 3.*th));
+%for i = 1:sz(1)
+    %for j = 1:sz(2)
+        %z = z1(i,j); 
+        %Smat = [1 + b(1)*z, b(2)*z, b(3)*z;...
+                %z*(b(1) - 1./2), 1 + b(2)*z, b(3)*z;...
+                %b(1)*z, z*(b(2) - 3./4), 1 + b(3)*z];
+        %s(i, j) = det(Smat);
+        %s = sum(b)*z + (z.^2).*((b(2)^2)/2. + (3*b(3)^2)/4.) ...
+                %+ (z.^3)*(prod(b) + (b(3)*(b(2)-3./4)*(b(1)-1./2)) ...
+                %+ (b(3)*b(2)*(b(1)-1./2)) + (b(3)*b(1)*(b(2) - 3./4)));
+    %end 
+%end 
+%(exp(complex(0, 3*th)) - 18.*exp(complex(0, 2*th)) ...
+%    +9.*exp(complex(0, th)) - 2)./(6.*exp(complex(0,3*th)));
+%(exp(complex(0, 2*th)) - 4.*exp(complex(0, th)) + 3.)/2.;
+%(exp(complex(0, 3*th)) - exp(complex(0, 2*th)))./(c1*exp(complex(0,2*th)) + c2*exp(complex(0, th)) + c3);
+
+%x = real(cos(th));
+%y = imag(sin(th));
+
+bsum = sum(b)
+bprod = prod(b)
+
+k = sin(xx).*yy
+
+s = (1 + b(1)*z).*(1 + b(2)*z).*(1 + b(3)*z) + (z.^3)*prod(b) ...
+    + (z.^3)*(b(3)*(b(2) - 3./4)*(b(1) - 1./2))...
+    - (b(3)*b(1)*z.^2).*(1+b(2)*z) - (b(2)*(b(1) - 1./2)*z.^2).*(1 + b(3)*z)...
+    - (b(3)*(b(2) - 3./4)*z.^2).*(1 + b(1)*z);
+    
+
+%smax = max(s, [], "all") 
+
+%[row, column] = find(abs(s) >= 1.0);
+%s(row, column) = 1.0;
+
+%smax = max(s, [], "all") 
+
+%contour(xx(row, column), yy(row, column), transpose(abs(s(row, column))));
+%surf(xx, yy, abs(s));
+[C1,h1]=contour(xx, yy, k);
+clabel(C1,h1)
